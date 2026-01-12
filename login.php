e4e1d0b101ee4aadcb98001a6a6f23f0f102767a
niranjansekarc <niranjansekarc@gmail.com>
2026-01-12 15:34:55 +0530

    login.php

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $conn = mysqli_connect("localhost", "root", "", "login_system");
-
 if (!$conn) {
     die("Database connection failed");
 }
@@ -12,9 +13,22 @@ $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'"
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) == 1) {
+
+    $_SESSION['username'] = $username;
+
+    $time = date("Y-m-d H:i:s");
+    mysqli_query($conn,
+        "INSERT INTO login_logs (username, login_time)
+         VALUES ('$username', '$time')"
+    );
+
     echo "Login successful";
+
 } else {
     echo "Invalid username or password";
 }
 ?>
+
+
+
 
